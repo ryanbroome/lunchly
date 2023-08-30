439735ebc94889477ac430dbe14c3afd172acd5f
Ryan Broome <ryan@MacBook-Pro.local>
2023-08-29 20:24:20 -0400

    browsing code

--- models/customer.js
+++ models/customer.js
@@ -26,7 +26,7 @@ class Customer {
        FROM customers
        ORDER BY last_name, first_name`
     );
-    return results.rows.map(c => new Customer(c));
+    return results.rows.map((c) => new Customer(c));
   }
 
   /** get a customer by ID. */
